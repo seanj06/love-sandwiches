88a232c77a3a29943f5ef9392aeb587a35a2eba7
seanj06 <seanj06@hotmail.com>
2022-11-07 03:41:39 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -23,7 +23,7 @@ def get_sales_data():
         print("Data should be 6 numbers, seperated by commas")
         print("Example: 10,20,30,40,50,60")
 
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here:\n ")
     
         sales_data = data_str.split(",")
 
