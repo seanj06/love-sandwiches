dd2bfa05fe89114da172e945965e13d5a3b35c41
seanj06 <seanj06@hotmail.com>
2022-11-07 02:47:48 +0000

    Refactor update worksheet functions into one

--- run.py
+++ run.py
@@ -52,23 +52,17 @@ def validate_data(values):
 
     return True          
 
-def update_sales_worksheet(data):
-    """
-    Update sales worksheet, add new row with the list data provided
-    """
-    print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet('sales')
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
 
-def update_surplus_worksheet(data):
+def update_worksheet(data, worksheet):
     """
-    Update surplus worksheet, add new row with the surplus data provided
-    """ 
-    print("Updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet('surplus') 
-    surplus_worksheet.append_row(data) 
-    print("Surplus worksheet updated successfully.\n") 
+    Receives a list of integers to be inserted into a worksheet
+    Update the worksheet with the relevant data provided
+    """
+    print(f"Updating {worksheet} worksheet ..\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated..\n")
+
 
 def calculate_surplus_data(sales_row):
     """
@@ -78,7 +72,7 @@ def calculate_surplus_data(sales_row):
     - Positive surplus indicates waste
     - Negative surplus indicates extra made when stock ran out
     """
-    print("Calculating surplus data")
+    print("Calculating surplus data\n")
     stock = SHEET.worksheet('stock').get_all_values()
     stock_row = stock[-1]
     
@@ -95,9 +89,9 @@ def main():
     """
     data =  get_sales_data()   
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data) 
+    update_worksheet(sales_data, "sales") 
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
 
 print("Welcome to love sandwiches Data Automation")
 main()    
